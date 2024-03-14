9d9c38683472f661432bd8dc6cbe3b654839d440
steel <gawct.231020@gmail.com>
2024-03-14 13:46:23 +0900

    P142作成

---
+++ Assets/Hole.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    //どのボールを吸い寄せるかをタグで指定
+    public string targetTag;
+
+    void OnTriggerStay(Collider other){
+        //コライダに触れているオブジェクトのRigidbodyコンポーネントを取得
+        Rigidbody r=other.gameObject.GetComponent<Rigidbody>();
+        //ボールがどの方向にあるかを計算
+        Vector3 direction=other.gameObject.transform.position-transform.position;
+        direction.Normalize();
+        //タグに応じてボールに力を加える
+        if(other.gameObject.tag==targetTag){
+            //中心地点でボールを止めるために速度を減速させる
+            r.velocity*=0.9f;
+            r.AddForce(direction*-20.0f,ForceMode.Acceleration);
+        }else{
+            r.AddForce(direction * 80f,ForceMode.Acceleration);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
